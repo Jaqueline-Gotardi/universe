29c3919a6ecf07c7cbdfe04386d4a08fe89a0269
Jaqueline Gotardi <jaquelineribeirogotardi@gmail.com>
2025-12-14 14:06:58 -0500

    feat: Configuração do react-router-dom e rotas bases (/login, /register, app/)

--- frontend-react/src/main.jsx
+++ frontend-react/src/main.jsx
@@ -3,8 +3,30 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+import { createBrowserRouter, Routerprovider } from 'react-router-dom' //importando o roteador
+
+//configurando o roteador
+const router = createBrowserRouter([
+  {
+    path: "/", //rota raiz
+    element: <App />, //layout principal ou o redirecionador
+  },
+  {
+    path: "/login",
+    element: <div>Página de login</div>,
+  },
+  {
+    path: "/register",
+    element: <div>Página de Cadastro</div>,
+  },
+  {
+    path: "/app",
+    element: <div>Dashboard Protegido</div>,
+  },
+]);
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Routerprovider router={router} />
   </StrictMode>,
 )
