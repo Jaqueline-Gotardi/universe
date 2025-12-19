2ec6216b3b92a487c9f05e9946a61cd01fcfde1d
Jaqueline Gotardi <jaquelineribeirogotardi@gmail.com>
2025-12-19 11:06:45 -0500

    refact: movi a lógica do fetch (q estava no useAuth.js) para o authContext.jsx, como tmb fiz a correção de variáveis.

--- frontend-react/contexts/authContext.jsx
+++ frontend-react/contexts/authContext.jsx
@@ -8,7 +8,6 @@ import { AuthContext } from "./contextStore";
     export function AuthProvider ({ children }) {
         const [ isAuthenticated, setAuthenticated ] = useState(false);
         const [ isLoading, setLoading ] = useState(true);
-
     //isAuthenticated e isLoading são os valores atuais
     //setIsAuthenticated e setIsLoading são para atualizar os valores atuais
       
@@ -17,12 +16,30 @@ import { AuthContext } from "./contextStore";
         const logout = () => setAuthenticated(false);
 
         useEffect(() => {
-            const verificarAutentificação = async () => {
+            const verificarAutentificação = async() => {
+                try {
 
-                setLoading(false);
+                    //tentar falar com os servidor e enviar o Cookie (cookie é uma informação sua q faz o site lembrar de vc)
+                    const response = await fetch (`/auth/verify`, 
+                        {credentials: "include"}
+                        //para garantir que o navegador anexe o cookie de autentificação. Se não incluirmos o credentials: "include", o Back não receberá o Cookie e sempre responderá 401 (não autorizado). O credentials: 'include' é a chave que permite o envio do Cookie de volta ao servidor para verificação
+                    );
+
+                    if (response.ok) {
+                        setAuthenticated(true);
+                    } else {
+                        setAuthenticated(false)
+                    }
+                } catch(error) {
+                    console.log('Erro de rede ao verificar Token', error)
+                    setAuthenticated(false)
+                } finally {
+                    setLoading(false) //parar de carregar
+                }
             }
             verificarAutentificação();
-        }, [] );
+        }, []);
+
     return (
     <AuthContext.Provider value = {{isAuthenticated, isLoading, login, logout}}>
     {children}
