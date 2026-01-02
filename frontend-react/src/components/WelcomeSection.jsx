0804e3d161520ae941fb650f9fabaf87e083c035
Jaqueline Gotardi <jaquelineribeirogotardi@gmail.com>
2026-01-02 16:36:21 -0500

    feat: implementação inicial de navegação entre telas no DashboardPage.jsx

--- frontend-react/src/components/WelcomeSection.jsx
+++ frontend-react/src/components/WelcomeSection.jsx
@@ -2,7 +2,8 @@ import "../style/tela-principal.css"
 import "../style/globals.css"
 import "../style/informacoes-extras.css"
 
-function WelcomeSection([onClicarIniciar, onClicarTutorial]) { //nome de componentes/funções em React deve-se começar com letras maiúsculas
+
+function WelcomeSection({onClicarIniciar, onClicarTutorial}) { //nome de componentes/funções em React deve-se começar com letras maiúsculas
     return (
     <section className="tela-principal" id="tela-principal-inicial" >
     <div className="text">
@@ -10,8 +11,13 @@ function WelcomeSection([onClicarIniciar, onClicarTutorial]) { //nome de compone
       <p>Selecione seu destino</p>
     </div>
     <div className="informacoes-extras">
-      <button type="button" className="btn-cosmic" id="btn-iniciar-exploracao" onClick={onClicarIniciar}>Iniciar Exploração</button>
-      <button type="button" className="btn-cosmic" id="btn-tutorial" onClick={onClicarTutorial}>Briefing da Nave</button>
+      <button type="button" className="btn-cosmic" id="btn-iniciar-exploracao" 
+      onClick={() => onClicarIniciar('tela-inicial')}>Iniciar Exploração</button>
+      
+      <button type="button" className="btn-cosmic" 
+      id="btn-tutorial" 
+      onClick={() => onClicarTutorial('tutorial')}>Briefing da Nave</button>
+
     </div> 
   </section> 
     ); 
--- frontend-react/src/pages/DashboardPage.jsx
+++ frontend-react/src/pages/DashboardPage.jsx
@@ -1,13 +1,20 @@
 /* dashboardPage.jsx (O Quarto VIP / Área Restrita): É o lugar onde o conteúdo real do site acontece. Só chega aqui quem passou pelo Vigia e provou que está autenticado. */
 
 import { useState } from "react";
+import WelcomeSection from "../components/WelcomeSection";
 
 function DashboardPage() { //nome de componentes/funções em React deve-se começar com letras maiúsculas
 
   const [ telaAtiva, setAtiva ] = useState("inicio");
 
     return (
-   <>
+      <>
+      {
+        telaAtiva === "inicio" &&
+         /* < WelcomeSection mudarTela = {setAtiva} / > */
+         < WelcomeSection onClicarIniciar={() => setAtiva('tela-inicial')} onClicarTutorial={() => setAtiva('tutorial')} />
+      }
+      
    </>
     ); 
 } 
