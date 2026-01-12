53d5c6a3d7309a840bafaf3aa4e782bf532a5d92
Jaqueline Gotardi <jaquelineribeirogotardi@gmail.com>
2026-01-11 19:03:57 -0500

    Implementado useEffect para auto-play ao trocar de música. Criado a função togglePlay para alternar estados de áudio. Adicionado funções nextTrack e prevTrack com lógica de loop (acabou uma música? inicia outra!). Tmb foi implementado handleIconClick com stopPropagation para alternar a visibilidade da barra musical. DomingOOOOUU

--- frontend-react/src/components/SoundTrack.jsx
+++ frontend-react/src/components/SoundTrack.jsx
@@ -1,12 +1,12 @@
 import "../style/loginPage.css"
 import { useState, useRef, useEffect } from "react";
 
-const song = [
+const songs = [
   { title:"Conexão cósmica: Frequênica 639Hz", src: "/musicas/639-hz-solfeggio-frequency-connect-with-higher-self-spiritual-music-160725.mp3" },
   { title: "Ecos do Espaço Profundo", src: "/musicas/69-spazio-ufo-universo-stelle-328185.mp3"},
   { title: "Meditação na Nebulosa", src: "/musicas/calm-space-music-312291.mp3"},
   { title: "Pulsares e Galáxias distantes", src: "/musicas/calm-space-music-312291.mp3"},
-  { title: "O canto dos Planetas", src: "/musicas/universe-planet-cosmic-galaxy-music-263133.mp3"},
+  { title: "O canto dos Planetas", src: "/musicas/universe-planet-cosmic-galaxy-music-263133.mp3"}, 
   { title: "Sinfonia do vazio Estelar", src: "/musicas/universe-space-sounds-3595.mp3"}
 ]; 
 
@@ -43,6 +43,50 @@ const SoundTrack = () => { //SoundTrack deve ser maíscula pq vai virar uma "tag
   },
   [currentTrack]
 );
+
+
+//esse useEffect é para garantir q se a música mudar, o novo áudio vai tocar, caso o player estiver no modo "tocando"
+useEffect(() => {
+  //se o audio atual estiver tocando (playing), ele é pego
+  if (audioRef.current && isPlaying) {
+    audioRef.current.play().catch(() => {});
+  }
+}, [currentTrack] //índice da música que foi pêga
+);
+
+//alternador do play
+const togglePlay = () => {
+  if (!audioRef.current) return; //retorna se não for o áudio atual
+
+  //mas, se o áudio atual estiver tocando:
+  if (isPlaying) {
+    audioRef.current.pause(); //pode pausar
+  } else {
+    audioRef.current.play().catch(() => {}); //e tocar
+  }
+  setIsPlaying(!isPlaying) //senão, o estado da música muda (não está tocando!)
+};
+
+//quando o ícone musical for clicado, a barra musical é exibida/ou escondida (é um alternador)
+const handleIconClick = (e)  => {
+  e.stopPropagation(); //trava o clique para não afetar oq está atrás
+  setShowBar((prev) => (!prev));  //prev é o valor anterior do estado, (!prev) inverte esse valor
+};
+
+//passar uma música
+const nextTrack = () => {
+  //muda o índice da música e prossegue para a próxima
+  setCurrentTrack((prev) => (prev + 1) % songs.length); //usam % tracks.length para criar um loop
+  setProgress(0); //se muda a música, reseta o progresso
+}
+
+//voltar uma música
+const prevTrack = () => {
+  setCurrentTrack((prev) => (prev -1 + songs.length) % songs.length);
+  setProgress(0);
+};
+
+
 }
 
 
