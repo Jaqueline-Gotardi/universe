90a4b96c17d57ff8bcfe123811c61dd5b99fe1fa
Jaqueline Gotardi <jaquelineribeirogotardi@gmail.com>
2026-01-29 10:05:36 -0500

    refactor: criação do menuData.js para guardar os dados dos botões

---
+++ frontend-react/src/data/menuData.js
@@ -0,0 +1,11 @@
+export const menuPrincipal = [
+    { id: "astros", label: "Informações Astronômicas", route: "/app/astros-menu", icon: "✨" },
+    { id: "planetas", label: "Informações Planetárias", route: "/app/planetary-menu", icon: "🪐"},
+    { id: "extras", label: "Informações Extras", route: "/app/extras-menu", icon: "🛰️"}, 
+];
+
+export const astrosMenu = [
+    { id: "constelações", label: "Constelações", route: "/app/constellations-menu", icon: ""},
+    { id: "eventos-astronomicos", label: "Eventos Astronômicos", route: "/app/events-menu", icon: ""},
+    { id: "galaxias", label: "Galáxias", route: "/app/galaxies-menu", icon: ""},
+]
