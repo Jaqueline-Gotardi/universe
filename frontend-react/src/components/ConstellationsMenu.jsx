cf75a646c8878434c4af36e5088d4aa3f9dc635b
Jaqueline Gotardi <jaquelineribeirogotardi@gmail.com>
2026-01-29 11:45:15 -0500

    refactor: Refatorando ConstellationsMenu.jsx para usar componentes cósmicos compartilhados

--- frontend-react/src/components/ConstellationsMenu.jsx
+++ frontend-react/src/components/ConstellationsMenu.jsx
@@ -1,4 +1,4 @@
-import { useNavigate } from "react-router-dom";
+/* import { useNavigate } from "react-router-dom";
 
 function ConstellationsMenu() {
     
@@ -22,4 +22,30 @@ function ConstellationsMenu() {
   </section>
     )
 }
-export default ConstellationsMenu;    
+export default ConstellationsMenu;      */
+
+import React from "react";
+import CosmicBackground from "./CosmicBackground";
+import CosmicCard from "./CosmicCard";
+import { useNavigate } from "react-router-dom";
+
+import { menuConstellations } from "../data/menuData";
+
+const ConstellationsMenu = () => {
+  const navigate = useNavigate();
+
+  return (
+    <div style={{position: "relative", minHeight: "100vh"}}>
+      <CosmicBackground />
+
+      <main>
+        <CosmicCard
+        title="✨ Constelações"
+        items={menuConstellations}
+        onBack={() => navigate("/app/astros-menu")}
+         />
+      </main>
+    </div>
+  )
+}
+export default ConstellationsMenu;
--- frontend-react/src/data/menuData.js
+++ frontend-react/src/data/menuData.js
@@ -9,3 +9,9 @@ export const menuAstros = [
     { id: "eventos-astronomicos", label: "Eventos Astronômicos", route: "/app/events-menu", icon: "🌑"},
     { id: "galaxias", label: "Galáxias", route: "/app/galaxies-menu", icon: "🌌"},
 ]
+
+export const menuConstellations = [
+    { id: "hemisferio-norte", label: "Hemisfério Norte", route: "/app/hemisferio-norte", icon: "🧭"},
+    { id: "hemisferio-sul", label: "Hemisfério Sul", route: "/app/hemisferio-sul", icon: "🌍"},
+    { id: "hemisferio-principal", label: "As famosas", route: "/app/constellations-famous", icon: "🌟"}
+]
