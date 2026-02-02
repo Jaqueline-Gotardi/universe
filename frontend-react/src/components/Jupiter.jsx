0d173aeb3d702dfd6a58b266fff313adb9e8359c
Jaqueline Gotardi <jaquelineribeirogotardi@gmail.com>
2026-02-02 11:36:44 -0500

    refactor: refatorando Mars.jsx e Jupiter.jsx para reutilizar o componente PlanetaTemplante.jsx

--- frontend-react/src/components/Jupiter.jsx
+++ frontend-react/src/components/Jupiter.jsx
@@ -1,29 +1,21 @@
 import { useNavigate } from "react-router-dom";
+import CosmicBackground from "./CosmicBackground";
+import { PlanetaTemplate } from "./PlanetaTemplate";
 
-function Jupiter() {
+const Jupiter = () => {
+  const navigate = useNavigate();
 
-    const navigate = useNavigate();
+  return (
+    <div style={{position: "relative", minHeight: "100vh"}}>
+      <CosmicBackground />
 
-    return (
-    <section className="tela-eventos-detalhe" id="detalhe-jupiter">
-    <div className="aurora-background"></div>
-    <div className="info-card" style={{position:"relative", zIndex:"1"}}>
-    <img src="/imagens/jupiter.png" alt="Jupiter" className="celestial-image" />
-      <h3 className="titulo-missao">Júpiter</h3>
-      <p className="descricao">
-        Júpiter, o maior planeta do Sistema Solar, um gigante gasoso com mais que o dobro da massa de todos os outros
-        planetas juntos.
-        <br />Possui uma marcante faixas de nuvens de cores vibrantes, causadas por tempestades e ventos extremamente
-        fortes. A mais famosa delas é a Grande Mancha Vermelha, uma tempestade colossal, maior que a Terra, que existe
-        há séculos.
-        <br />Júpiter tem dezenas de luas, incluindo as quatro grandes luas galileanas (Io, Europa, Ganímedes e Calisto),
-        que são visíveis com pequenos telescópios. Ele atua como um "aspirador de pó" cósmico, protegendo o sistema
-        interior de cometas e asteroides.
-      </p>
+      <main>
+        <PlanetaTemplate
+        planetaId= "jupiter"
+        onBack={() => navigate("/app/constellations-menu")}
+        />
+      </main>
     </div>
-    <button type="button" className="botao-voltar-planeta" id="btn-voltar-jupiter"
-    onClick={() => navigate("/app/planetary-information")}>Voltar</button>
-  </section>
-    )
+  )
 }
-export default Jupiter;
+export default Jupiter; 
--- frontend-react/src/components/Mars.jsx
+++ frontend-react/src/components/Mars.jsx
@@ -1,27 +1,21 @@
 import { useNavigate } from "react-router-dom";
+import CosmicBackground from "./CosmicBackground";
+import { PlanetaTemplate } from "./PlanetaTemplate";
 
-function Mars() {
+const Mars = () => {
+  const navigate = useNavigate();
 
-    const navigate = useNavigate();
+  return (
+    <div style={{position: "relative", minHeight: "100vh"}}>
+      <CosmicBackground />
 
-    return (
-    <section className="tela-eventos-detalhe" id="detalhe-marte">
-    <div className="aurora-background"></div>
-    <div className="info-card" style={{position:"relative", zIndex:"1"}}>
-    <img src="/imagens/marte.png" alt="Marte" className="celestial-image" />
-      <h3 className="titulo-missao">Marte</h3>
-      <p className="descricao">
-        Marte é o quarto planeta a partir do Sol e é amplamente conhecido como o "planeta vermelho" por sua coloração
-        avermelhada, causada pela ferrugem de minerais ricos em ferro em sua superfície.
-        <br />É um planeta rochoso, com imensos desfiladeiros, vastas planícies e as maiores montanhas vulcânicas do nosso
-        sistema.
-        <br />Apesar de sua atmosfera ser fina e fria, Marte possui calotas polares de gelo e evidências de que já abrigou
-        água em seu passado, o que o torna um dos principais alvos de busca por vida no espaço.
-      </p>
+      <main>
+        <PlanetaTemplate
+        planetaId= "marte"
+        onBack={() => navigate("/app/constellations-menu")}
+        />
+      </main>
     </div>
-    <button type="button" className="botao-voltar-planeta" id="btn-voltar-marte"
-    onClick={() => navigate("/app/planetary-information")}>Voltar</button>
-  </section>
-    )
+  )
 }
-export default Mars;
+export default Mars; 
