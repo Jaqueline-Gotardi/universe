b28dc951aab82fd8bee3d3be31a51a12be58143e
Jaqueline Gotardi <jaquelineribeirogotardi@gmail.com>
2026-03-13 17:40:26 -0500

    feat: iniciando estruturação do agente badge

---
+++ frontend-react/src/components/AgentBadge.jsx
@@ -0,0 +1,27 @@
+import { useNavigate } from "react-router-dom"
+
+export const AgentBadge = () => {
+  const navigate = useNavigate();
+
+  const styles = {
+
+  }
+
+  return (
+    <div style={styles.container} onClick={() => navigate("/profile")}>
+      <div style={styles.brilho}>
+
+        <div>
+          <div style={styles.avatarWrapper}>
+        <img alt="avatar" style={styles.avatar}/>
+        <div style={styles.anel} />
+        </div>
+        <div style={styles.info}>
+
+        </div>
+      </div>
+
+      </div>
+    </div>
+  )
+}
