6424be29d43d174249913db6e3edb000b8948f1e
Jaqueline Gotardi <jaquelineribeirogotardi@gmail.com>
2026-04-03 13:37:41 -0500

    refactor: finalização da refatoração dos componentes de deep-space

--- frontend-react/src/components/deep-space/StarClusters.jsx
+++ frontend-react/src/components/deep-space/StarClusters.jsx
@@ -7,29 +7,12 @@ import { PlanetaTemplate } from "../solar-system/PlanetaTemplate";
 const StarClusters = () => {
   return (
   
-  <section className="tela-eventos-detalhe" id="detalhe-agrupamentos-estelares" style={{position: "relative", minHeight: "100vh"}}>
+  <section className="telaDetalhe" id="detalhe-agrupamentos-estelares">
     <div className="space-background">
       <div className="stars"></div>
       </div> 
       
-      <main className="container-star-clusters">
-       <style>{`
-        .container-star-clusters img {
-        width: 100%; 
-        max-width: 800px;       
-        transform: scale(1.1);      
-        transition: all 0.3s ease;
-        object-fit: contain;
-        margin: 0px; 
-        border-radius: 25px;
-        }
-        @media (max-width: 768px) {
-        .container-big-dipper img {
-        width: 100%;
-        transform: scale(1);
-        }
-        `}
-        </style>
+      <main className="containerAstro">
 
        <PlanetaTemplate
         planetaId= "agrupamentos-estelares"
--- frontend-react/src/components/deep-space/Supernovas.jsx
+++ frontend-react/src/components/deep-space/Supernovas.jsx
@@ -7,29 +7,12 @@ import { PlanetaTemplate } from "../solar-system/PlanetaTemplate";
 const Supernovas = () => {
   return (
   
-  <section className="tela-eventos-detalhe" id="detalhe-supernovas" style={{position: "relative", minHeight: "100vh"}}>
+  <section className="telaDetalhe" id="detalhe-supernovas">
     <div className="space-background">
       <div className="stars"></div>
       </div> 
       
-      <main className="container-supernovas">
-       <style>{`
-        .container-supernovas img {
-        width: 100%; 
-        max-width: 800px;       
-        transform: scale(1.1);      
-        transition: all 0.3s ease;
-        object-fit: contain;
-        margin: 0px; 
-        border-radius: 25px;
-        }
-        @media (max-width: 768px) {
-        .container-big-dipper img {
-        width: 100%;
-        transform: scale(1);
-        }
-        `}
-        </style>
+      <main className="containerAstro">
 
        <PlanetaTemplate
         planetaId= "supernovas"
