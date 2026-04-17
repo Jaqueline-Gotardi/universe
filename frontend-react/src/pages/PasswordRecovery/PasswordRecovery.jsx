723bd2e12e00af14a7116bb359bd71f2e71e8569
Jaqueline Gotardi <jaquelineribeirogotardi@gmail.com>
2026-04-17 15:22:46 -0500

    feat: implementado tela de 'Recuperar Senha' + estilização com css module

--- frontend-react/src/pages/PasswordRecovery/PasswordRecovery.jsx
+++ frontend-react/src/pages/PasswordRecovery/PasswordRecovery.jsx
@@ -1,6 +1,68 @@
-import styles from "./PasswordRecovery.module.css"
+import { useState } from "react";
+import { NavLink } from "react-router-dom";
+import CosmicBackground from "../../components/layout/CosmicBackground";
+import styles from "./PasswordRecovery.module.css";
 
 function PasswordRecovery() {
+  const [email, setEmail] = useState("");
+  const [submitted, setSubmitted] = useState(false);
 
+  async function handleRecover(event) { //função para disparar o pedido de recuperar senha, via email
+    event.preventDefault();
+    try {
+      console.log("Solicitando recuperação para:", email);
+
+      //simular sucesso do email enviado
+      setSubmitted(true);
+    } catch (error) {
+      alert("Erro na transmissão galática. Tente novamente.");
+    }
+  }
+
+  return (
+  <section className={styles.telaRecuperarSenha}>
+    <CosmicBackground />
+    <div className={styles.containerRecuperarSenha}>
+      
+      {!submitted ? ( //se o email não for enviado, mostrar o formulário para recuperar senha
+       <>
+        <h2 className={styles.tituloSecao}>Resgatar Senha</h2>
+        <p className={styles.descricao}>
+          Insira seu e-mail de agente para receber as coordenadas de redefinição.
+          </p>
+          
+          <form className={styles.formRecuperarSenha} onSubmit={handleRecover}>
+            <div className={styles.campoInput}>
+              <label htmlFor="email-recuperar-senha">E-mail de Cadastro</label>
+              <input 
+                type="email" 
+                id="email-recuperar-senha" 
+                placeholder="seu-email@universo.com"
+                value={email} 
+                onChange={(e) => setEmail(e.target.value)} 
+                required
+                />
+            </div>
+            
+            <button type="submit" className={styles.btnRecuperarSenha}>
+              Enviar Instruções
+            </button>
+          </form>
+          </>
+          ) : (  //se o email for enviado, mostrar informações de ok
+          <div className={styles.sucessoContainer}>
+            <h2 className={styles.tituloSecao}>E-mail Enviado!</h2>
+            <p className={styles.descricao}>
+              Verifique sua caixa de entrada. As instruções de recuperação já cruzaram a atmosfera!
+            </p>
+          </div>
+        )}
+        
+        <p className={styles.linkVoltar}>Lembrou a senha? 
+          <NavLink to="/login"> Voltar para a base</NavLink>
+        </p>
+      </div>
+    </section>
+  );
 }
 export default PasswordRecovery;
