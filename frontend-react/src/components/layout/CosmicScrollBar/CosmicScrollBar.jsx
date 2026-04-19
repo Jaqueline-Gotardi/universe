a33508506acbdfb932f34a904a71fbd45b2c867e
Jaqueline Gotardi <jaquelineribeirogotardi@gmail.com>
2026-04-19 16:45:19 -0500

    chore: removido lógica patuleia de ocultar a CosmicScrollBar antes da autenticação, pois o componente já é importando dentro da WelcomeSection

--- frontend-react/src/components/layout/CosmicScrollBar/CosmicScrollBar.jsx
+++ frontend-react/src/components/layout/CosmicScrollBar/CosmicScrollBar.jsx
@@ -1,12 +1,6 @@
-import useAuth from "../../../hooks/useAuth";
 import styles from "./CosmicScrollBar.module.css"
 
 const CosmicScrollBar = () => {
-    const { isAuthenticated, isLoading } = useAuth();
-    
-    //se está carregando ou não está logado não mostra a barra de rolagem do topo
-    if (isLoading || !isAuthenticated) return null;
-
     return <div className={styles.cosmicScrollBar}></div>;
 };
 
